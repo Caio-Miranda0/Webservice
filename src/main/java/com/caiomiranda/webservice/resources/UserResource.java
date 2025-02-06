7b6ed22ac85b88f38bb3d6b81ad38cdef295e0a6
Caio Charles <caio.miranda@estudante.ifb.edu.br>
2025-02-06 16:06:57 -0300

    Entidade User criada e seu resource com teste

---
+++ src/main/java/com/caiomiranda/webservice/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.caiomiranda.webservice.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.caiomiranda.webservice.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1L, "Caio", "caio@email.com", "+5561990728989", "abc123");
+		return ResponseEntity.ok().body(user);
+	}
+}
